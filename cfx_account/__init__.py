ab4734338343ab1514176aa84b23eec7fd4766a5
PanaW <pan.wang@confluxnetwork.org>
2021-10-09 15:03:44 +0800

    lock dependencies version

---
+++ cfx_account/__init__.py
@@ -0,0 +1,5 @@
+from cfx_account.account import Account
+
+__all__ = [
+    "Account"
+]
--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.0.1'
+VERSION = '0.0.2'
 DESCRIPTION = 'Conflux account'
 LONG_DESCRIPTION = 'Conflux account that can sign a conflux transaction'
 
@@ -15,7 +15,7 @@ setup(
     long_description=LONG_DESCRIPTION,
     packages=find_packages(),
     install_requires=[
-        "eth-account>=0.5.3,<0.6.0",
+        "eth-account>=0.5.3,<=0.5.5",
         "cfx-address"
     ],  # add any additional packages that
     # needs to be installed along with your package. Eg: 'caer'
