75b0556c29db0a7120dcf73dfd3f845d54451d50
Zhang Wenda <wenda.zhang@confluxnetwork.org>
2022-08-03 15:39:17 +0800

    Switch eth-account version. Tests to be done

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.0.4'
+VERSION = '0.1.0-beta.1'
 DESCRIPTION = 'Conflux account'
 LONG_DESCRIPTION = 'Conflux account that can sign a conflux transaction'
 
@@ -15,7 +15,7 @@ setup(
     long_description=LONG_DESCRIPTION,
     packages=find_packages(),
     install_requires=[
-        "eth-account>=0.5.3,<=0.5.4",
+        "eth-account>=0.6.0,<0.7.0",
         "cfx-address"
     ],  # add any additional packages that
     # needs to be installed along with your package. Eg: 'caer'
