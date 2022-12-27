1c60e4fdb7d66562199c1290c0fdf39bd1be6644
Darwin <wenda.zhang@confluxnetwork.org>
2022-12-27 15:40:28 +0800

    doc: change legacy EIP-712 documents and variable name to CIP23

--- cfx_account/_utils/structured_data/validation.py
+++ cfx_account/_utils/structured_data/validation.py
@@ -16,23 +16,23 @@ from eth_account._utils.structured_data.validation import (
 )
 
 def validate_CIP23Domain_schema(structured_data: Dict[str, Any]):
-    # Check that the `types` attribute contains `EIP712Domain` schema declaration
+    # Check that the `types` attribute contains `CIP23Domain` schema declaration
     if "CIP23Domain" not in structured_data["types"]:
         raise ValidationError("`CIP23Domain struct` not found in types attribute")
-    # Check that the names and types in `EIP712Domain` are what are mentioned in the EIP-712
+    # Check that the names and types in `CIP23Domain` are what are mentioned in the CIP-23
     # and they are declared only once (if defined at all)
-    EIP712Domain_data = structured_data["types"]["CIP23Domain"]
-    header_fields = used_header_fields(EIP712Domain_data)
+    CIP23Domain_data = structured_data["types"]["CIP23Domain"]
+    header_fields = used_header_fields(CIP23Domain_data)
     if len(header_fields) == 0:
         raise ValidationError(f"One of {EIP712_DOMAIN_FIELDS} must be defined in {structured_data}")
     for field in header_fields:
-        validate_field_declared_only_once_in_struct(field, EIP712Domain_data, "CIP23Domain")
+        validate_field_declared_only_once_in_struct(field, CIP23Domain_data, "CIP23Domain")
 
 
 def validate_structured_data(structured_data: Dict[str, Any]):
     # validate the `types` attribute
     validate_types_attribute(structured_data)
-    # validate the `EIP712Domain` struct of `types` attribute
+    # validate the `CIP23Domain` struct of `types` attribute
     validate_CIP23Domain_schema(structured_data)
     # validate the `primaryType` attribute
     validate_primaryType_attribute(structured_data)
