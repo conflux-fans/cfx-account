0ba84b803e3f45347332ad33d57162b02697c6d7
darwintree <17946284+darwintree@users.noreply.github.com>
2024-10-30 11:53:45 +0800

    test: fix tests for hexbytes version greater than 1.2.0

--- tests/cip1559_transaction_test.py
+++ tests/cip1559_transaction_test.py
@@ -1,6 +1,7 @@
 from cfx_address import Base32Address
 from cfx_account import Account
 from cfx_account._utils.transactions.cip1559_transactions import CIP1559Transaction
+from tests.test_utils import assert_hex_equal
 
 signed_cip1559_transaction_dict = {
     "type": 2,
@@ -56,11 +57,14 @@ unsigned_cip1559_transaction_dict = {
 
 def test_cip1559_transaction_hashing_and_encoding():
     cip1559_transaction = CIP1559Transaction(signed_cip1559_transaction_dict)
-    assert (
-        cip1559_transaction.hash().hex()
-        == "3da56dbe2b76c41135c2429f3035cd79b1abb68902cf588075c30d4912e71cf3"
+    assert_hex_equal(
+        cip1559_transaction.hash(),
+        "3da56dbe2b76c41135c2429f3035cd79b1abb68902cf588075c30d4912e71cf3"
     )
-    assert cip1559_transaction.encode().hex() == "63667802f869f864646464649419578cf3c71eab48cf810c78b5175d5c9e6ef441646464648c48656c6c6f2c20576f726c64f838f79419578cf3c71eab48cf810c78b5175d5c9e6ef441e1a01234567890abcdef1234567890abcdef1234567890abcdef1234567890abcdef800101"
+    assert_hex_equal(
+        cip1559_transaction.encode(),
+        "63667802f869f864646464649419578cf3c71eab48cf810c78b5175d5c9e6ef441646464648c48656c6c6f2c20576f726c64f838f79419578cf3c71eab48cf810c78b5175d5c9e6ef441e1a01234567890abcdef1234567890abcdef1234567890abcdef1234567890abcdef800101"
+    )
 
 def test_signing_cip1559_transaction():
     acct = Account.create()
--- tests/mnemonic_test.py
+++ tests/mnemonic_test.py
@@ -1,11 +1,12 @@
 from cfx_account import Account
+from tests.test_utils import assert_hex_equal
 
 mnemonic = 'faint also eye industry survey unhappy boil public lemon myself cube sense'
 private_key_0 = "0x40d0f137665463584cc57fce2b761572a85d1cbf1601fc93d001c129b2a11c92"
 
 def test_generate_from_mnemonic():
     acct = Account.from_mnemonic(mnemonic, network_id=1)
-    assert acct.key.hex() == private_key_0
+    assert_hex_equal(acct.key, private_key_0)
     assert acct.address == 'cfxtest:aargrnff46pmuy2g1mmrntctkhr5mzamh6nmg361n0'
     
 def test_account_restore():
